4a604c183458e02d24d009c69df40bb2670bb027
Nuno Guterres Nogueira <guterresnogueira@gmail.com>
2025-03-19 20:07:20 +0100

    it works, does the job.

--- index.js
+++ index.js
@@ -2,25 +2,62 @@
 // https://www.assemblyscript.org/compiler.html#programmatic-usage
 
 import { program } from "commander"
+import got from "got";
 import asc from "assemblyscript/asc";
 import crypto from "crypto"
+import fs from "fs"
 
-program.option('--example').argument('<string>');
+import dotenv from "dotenv"
+dotenv.config()
+
+program.option('--sign');
 program.parse();
 
 const options = program.opts();
-console.log(`${options.example} : ${program.args}`)
 
-const { error, stdout, stderr, stats } = await asc.main([
-  // Command line options
-  "assembly/index.ts",
-  "--outFile", "assembly/build/main.wasm",
-  "--optimize",
-  "--stats"
-]);
-if (error) {
-  console.log("Compilation failed: " + error.message);
-  console.log(stderr.toString());
-} else {
-  console.log(stdout.toString());
+if (options.sign) {
+  const error = await compileAssemblyScript('./assembly/index.ts')
+  if (!error) {
+    const binary = hashWasmFile(process.env.AS_FILE)
+    const response = await postApplication(binary)
+
+    console.log(`Status Code: ${response.statusCode} Body: ${JSON.stringify(response.body)}`)
+  }
+}
+
+// -------------------------- //  // -------------------------- // // -------------------------- //
+// -------------------------- //  // -------------------------- // // -------------------------- //
+// -------------------------- //  // -------------------------- // // -------------------------- //
+
+async function compileAssemblyScript(pathToIndex) {
+  const { error, stdout, stderr, stats } = await asc.main([pathToIndex, "--optimize"]);
+  return error ? error : 0
+}
+
+function hashWasmFile(pathToWasmFile) {
+  if (!fs.statSync(pathToWasmFile)) {
+    return
+  }
+  const wasmFile = fs.readFileSync(pathToWasmFile)
+
+  let dataToHash = Buffer.alloc(512 + 4, 0x00) // in order: config, size
+  dataToHash = Buffer.concat([dataToHash, wasmFile]);
+  dataToHash.writeUInt32LE(wasmFile.length, 512) // Write size of main.wasm
+
+  const hash = crypto.createHmac('sha256', process.env.HMAC_SECRET).update(dataToHash).digest()
+
+  return Buffer.concat([hash, dataToHash]);
+}
+
+async function postApplication(data) {
+  const response = await got.post(`${process.env.SERVER_URL}/firmware/${process.env.DEVICE_ID}`, {
+    body: data,
+    responseType: 'json',
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Content-Length': data.length,
+    },
+  });
+
+  return response
 }
