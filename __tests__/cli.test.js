5af9e10348d431e9b0a8f0654c197689a723871d
Nuno Guterres Nogueira <guterresnogueira@gmail.com>
2025-05-08 19:00:03 +0200

    .

--- __tests__/cli.test.js
+++ __tests__/cli.test.js
@@ -1,7 +1,7 @@
 import { jest } from '@jest/globals';
 import { execSync } from 'child_process';
 import got from "got";
-import { signApp, postApplication, compileAssemblyScript } from "../dist/build"
+import { signApp, postApplication } from "../dist/deploy"
 import { checkEnvVariables } from "../dist/utils/config"
 import { Result } from '../dist/utils/result';
 
